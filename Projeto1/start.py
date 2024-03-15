24be838087b7580ec29381190ed669113b20c9eb
rebekacardososena <bekacristine@gmail.com>
2024-03-15 17:19:42 -0300

    iniciante em Python

---
+++ Projeto1/start.py
@@ -0,0 +1,88 @@
+
+# personagem_nome = 'Rebeka'
+# personagem_idade = '26'
+# personagem_esporte = 'Futebol'
+# personagem_jogo = 'Fortnite'
+#
+# print('Meu nome é ' + personagem_nome + ',')
+# print('eu tenho ' + personagem_idade +' anos.')
+# print('Meu esporte favorito é o ' + personagem_esporte + ',')
+# print('eu gosto de jogar ' + personagem_jogo +  ' nas minhas horas vagas.')
+
+# nome = 'Marcos da Silva'
+# print(nome.count('a'))
+# print(nome.upper())
+# print(nome[4])
+
+
+# print(6)
+# print(-8)
+# print(8.290)
+# print(4+2)
+# print(4+2.345)
+# print(2 * (2 + 5))
+#
+# num1 = 5
+# num2 = 10
+#
+# print(str(num1) + ' e o meu numero da sorte')
+
+# num1 = 52
+# num2 = 10
+#
+# print(min(2, 8))
+# print(max(2, 8))
+# print(pow(2, 3))
+# print(round(11.7))
+
+#String
+# nome = input('Digite o seu nome: ')
+# idade = input('Digite sua idade: ')
+#
+# print('Olá ' + nome + '!' + ' Hoje você tem ' + idade + ' anos de idade')
+
+#Float e int
+# num1 = input('Digite o primeiro numero a ser somado: ')
+# num2 = input('Digite o segundo numero a ser somado: ')
+# resultado = float(num1) + float(num2)
+#
+# print(resultado)
+
+#List
+#amigos = ['Marcos', 'Pedro', 'Adriana', 'Karina', 'Julia']
+# print(amigos[3])
+# print(amigos[1:4])#Do item 1 ao 4
+# print(amigos[2:])#Menos o item2
+# amigos[1] = 'Joao'#Alterando o item1 para Joao
+# print(amigos)
+
+#Listfunctions
+
+# numeros = [3, 5, 12, 2, 15, 6]
+# amigos = ['Marcos', 'Pedro', 'Adriana', 'Karina', 'Julia', 'Julia', 'Julia']
+
+#amigos.pop(3)#Remove pela posicao do item
+#amigos.remove('Adriana')#Remove o item pelo nome
+#amigos.insert(1,'Joao')#Adiciona na posicao 1
+#amigos.append('Joao')#Adicioando mais um item ao final da lista
+#amigos.extend(numeros)#Juntando a lista amigos com numeros
+#print(amigos.index('Julia'))#Mostra a posicao na lista pelo nome do item
+#print(amigos.count('Julia'))#Mostra quantos item tem como esse.
+
+# amigos.sort()#Organiza a lista de amigos por ordem alfabetica
+# print(amigos)
+
+# numeros.sort()#Organiza em ordem numerica
+# print(numeros)
+
+# #IfElse
+#
+# acordei_antes_9 = False
+# fome = True
+#
+# if acordei_antes_9 and fome:
+#     print('Hora de fazer Café da Manhã')
+# elif acordei_antes_9 and not(fome):
+#     print('Hora de fazer uma Corrida')
+# else:
+   # print('Hora de fazer o Almoço')
